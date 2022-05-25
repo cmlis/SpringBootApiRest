2b55b15c999f3612cd9ee756ca3436bd1516bac5
Camila Santana <cssantana8@gmail.com>
2022-05-25 09:52:50 -0600

    Introducao

---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,55 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotEmpty;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+
+public class TopicoForm {
+
+	@NotNull @NotEmpty 
+	private String titulo;
+	
+	@NotNull @NotEmpty 
+	private String mensagem;
+	
+	@NotNull @NotEmpty
+	private String nomeCurso;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	
+	public Topico converter(CursoRepository repository) {
+		
+		Curso curso = repository.findByNome(nomeCurso);
+		
+		return new Topico(titulo,mensagem, curso);
+	}
+
+
+
+	
+	
+	
+}
